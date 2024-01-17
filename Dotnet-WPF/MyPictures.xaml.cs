1aac830cf17cc37abbf1d77ddb1d7861ad9b01d1
AhnYeonghoo <dksdudgn00@naver.com>
2024-01-18 08:53:25 +0900

    Photo List

---
+++ Dotnet-WPF/MyPictures.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dotnet_WPF
+{
+    /// <summary>
+    /// Interaction logic for MyPictures.xaml
+    /// </summary>
+    public partial class MyPictures : Window
+    {
+        public MyPictures()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Photo
+    {
+        public string Source { get; }
+        public Photo(string path)
+        {
+            Source = path;
+        }
+        public override string ToString() => Source;
+    }
+}
